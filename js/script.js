4f3861b82019f57416c00868ae2fc6241ba246b5
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2019-08-07 00:54:00 -0700

    Final Project Finished!
    go to brandonaaronson.com for the experience.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 const portfolio = document.querySelector('#port-btn');
 const textBox = document.querySelector('#text-box');
 const logo = document.querySelector('.ba-logo')
-const overlay = document.querySelector('#overlay');
+const portfolioBtn = document.querySelector('#port-btn')
+const overlay = document.querySelector('.overlay');
+const overlayClose = document.querySelector('.overlay-close')
+
+// -- open overlay w/ nav button -- //
 
 portfolio.addEventListener('click', function() {
   if (overlay.classList.contains('hidden')) {
     overlay.classList.remove('hidden');
-    logo.classList.add('hidden');
+    portfolioBtn.classList.add('hidden');
   } else {
     overlay.classList.add('hidden');
-    logo.classList.remove('hidden');
   }
 });
 
+
+// -- close the overlay -- //
+
+document.addEventListener('click', (e) => {
+  if (e.target.className == 'overlay' || e.target.className == 'overlay-close') {
+    overlay.classList.add('hidden');
+    portfolioBtn.classList.remove('hidden');
+  }
+});
+
+document.addEventListener('keydown', (e) =>{
+  if (e.key === "Escape") {
+    overlay.classList.add('hidden');
+    portfolioBtn.classList.remove('hidden');
+  }
+});
